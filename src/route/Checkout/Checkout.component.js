a1ba44a2017ceb476894f1a438438c146f6101ed
judabne <76679098+judabne@users.noreply.github.com>
2021-04-28 19:38:50 +0300

    Omitted a repeated part of the code

--- src/route/Checkout/Checkout.component.js
+++ src/route/Checkout/Checkout.component.js
@@ -75,34 +75,6 @@ class Checkout extends SourceCheckout {
             </main>
         );
     }
-
-    render() {
-        return (
-            <main block="Checkout">
-                { this.renderProgress() }
-                <ContentWrapper
-                  wrapperMix={ { block: 'Checkout', elem: 'Wrapper' } }
-                  label={ __('Checkout page') }
-                >
-                    { this.renderSummary(true) }
-                    
-                    <div block="Checkout" elem="Step">
-                        { this.renderTitle() } 
-                        { this.renderGuestForm() }
-                        { this.renderStep() }
-                        { this.renderLoader() }
-                        
-                    </div>
-                    <div>
-                        { this.renderSummary() }
-                        { this.renderPromo() }
-                        { this.renderCoupon() }
-                        
-                    </div>
-                </ContentWrapper>
-            </main>
-        );
-    }
 }
 
 export default Checkout;
